c4e68b8dc5e71e73fc139f46aecdbca1ef84a558
rajesh.s <rajeshswarna123@gmail.com>
2018-12-27 16:54:03 +0530

    Initial commit

---
+++ ContactService/ContactService.cs
@@ -0,0 +1,61 @@
+﻿using ContactService.DataServices;
+using ContactService.Models;
+using Services.Automapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Extensions;
+
+namespace ContactService
+{
+     public class ContactServices
+    {
+        public class ContctService : IContactService
+        {
+
+            public AddressBookDB context { get; set; }
+            
+            public ContctService(AddressBookDB _context)
+            {
+                this.context = _context;
+            }
+
+            #region Contact Service(s)
+
+            public Models.Contact CreateContact(DataServices.Contact contact)
+            {
+                context.Insert(contact);
+                return contact.MapTo<DataServices.Contact, Models.Contact>();
+            }
+
+            public int DeleteContact(int Id)
+            {
+                context.Execute("Delete From Contact Where Id = @0", Id);
+                return Id;
+            }
+
+            public Models.Contact UpdateContact(DataServices.Contact contact)
+            {
+                context.Update(contact);
+                return contact.MapTo<DataServices.Contact, Models.Contact>();
+            }
+
+            public Models.Contact GetContact(int Id)
+            {
+                var sql = PetaPoco.Sql.Builder.Select("*").From("Contact").Where("Id=@0", Id);
+                return (context.FirstOrDefault<DataServices.Contact>(sql)).MapTo<DataServices.Contact, Models.Contact>();
+            }
+
+            public List<Models.Contact> GetContacts()
+            {
+                var sql = PetaPoco.Sql.Builder.Select("*").From("Contact");
+                return (context.Fetch<DataServices.Contact>(sql)).MapToCollection<DataServices.Contact,Models.Contact>();
+            }
+
+            #endregion
+
+        }
+    }
+}
---
+++ ContactService/IContactService.cs
@@ -0,0 +1,19 @@
+﻿using ContactService.DataServices;
+using ContactService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactService
+{
+    public interface IContactService
+    {
+        List<Models.Contact> GetContacts();
+        Models.Contact UpdateContact(DataServices.Contact contact);
+        Models.Contact GetContact(int Id);
+        Models.Contact CreateContact(DataServices.Contact contact);
+        int DeleteContact(int Id);
+    }
+}
